c62fb9078fffbffad3cd97efdb0968c0f679bff4
Alex <alexander.ogier@gmail.com>
2024-12-01 18:08:29 +1300

    day1

--- day1/part1.js
+++ day1/part1.js
@@ -1,3 +1,16 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n').map(l => l.split(/[ ]+/));
+
+var first = input.map(i => parseInt(i[0])).sort();
+var second = input.map(i => parseInt(i[1])).sort();
+
+console.log(first, second);
+
+var dist = 0;
+
+first.forEach((f, fi) => {
+    dist += Math.abs(f - second[fi]);
+})
+
+console.log(dist);
--- day1/part2.js
+++ day1/part2.js
@@ -1,3 +1,22 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n').map(l => l.split(/[ ]+/));
+
+var left = input.map(i => parseInt(i[0])).sort();
+var right = input.map(i => parseInt(i[1])).sort();
+
+var score = 0;
+
+left.forEach((l) => {
+    var occurs = 0;
+
+    right.forEach(r => {
+        if (l == r) {
+            occurs++;
+        }
+    });
+
+    score += occurs * l;
+})
+
+console.log(score);
