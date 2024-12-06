a81c0fb5ef094bff6567447f35b95333192a12a7
Alex <alexander.ogier@gmail.com>
2024-12-06 23:16:33 +1300

    improve perf

--- day6/part2better.js
+++ day6/part2better.js
@@ -3,8 +3,6 @@ var fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n').map(n => n.split(''));
 
-var obs = {};
-
 var ds = [
     [0, -1],
     [1, 0],
@@ -17,24 +15,32 @@ var maxx = input[0].length - 1;
 var miny = 0;
 var maxy = input.length - 1;
 
+var obs = new Array(input.length*input[0].length);
+
 var startx = 0;
 var starty = 0;
 
-const k = (x, y) => {
+const ko = (x, y) => {
     return y*(maxx+3) + x+1; // padded
 }
 
+const kv = (x, y, d = 0) => {
+    return 4*(y*(maxx+3) + x+1) + d; // padded
+}
+
 const rk = (v) => {
+    var d = v % 4;
+    v = Math.floor(v / 4);
     var x1 = v % (maxx+3);
     var y = Math.floor(v / (maxx+3));
-    return [ x1-1, y];
+    return [ x1-1, y, d];
 }
 
 input.forEach((r, y) => {
     r.forEach((c, x) => {
         switch(c) {
             case '#':
-                obs[k(x, y)] = true;
+                obs[ko(x, y)] = true;
             break;
             case '^':
                 startx = x;
@@ -42,10 +48,12 @@ input.forEach((r, y) => {
             break;
         }
     })
-})
+});
 
-function runSim(ox, oy, first) {
-    if (obs[k(ox, oy)] || (ox == startx && oy == starty)) {
+var visited = new Array(4*input.length*input[0].length);
+
+function runSim(ox, oy, first, iter) {
+    if (obs[ko(ox, oy)] || (ox == startx && oy == starty)) {
         return false;
     }
 
@@ -53,18 +61,20 @@ function runSim(ox, oy, first) {
     var gy = starty;
     var d = 0;
 
-    var visited = {};
-
     while(gx <= maxx && gx >= minx && gy <= maxy && gy >= miny) {
-        if (visited[k(gx, gy)] == d+5) {
-            return true;
+        if (first) {
+            visited[kv(gx, gy, 0)] = iter;
+        } else {
+            if (visited[kv(gx, gy, d)] == iter) {
+                return true;
+            }
+            visited[kv(gx, gy, d)] = iter;
         }
-        visited[k(gx, gy)] = d+5;
 
         var nx = gx + ds[d][0];
         var ny = gy + ds[d][1];
 
-        while (obs[k(nx, ny)] || (nx == ox && ny == oy)) {
+        while (obs[ko(nx, ny)] || (nx == ox && ny == oy)) {
             d++;
             d = d % 4;
             nx = gx + ds[d][0];
@@ -79,14 +89,14 @@ function runSim(ox, oy, first) {
 }
 
 
-var init = runSim(-5, -5, true);
+var init = runSim(-5, -5, true, 1);
 
 var count = 0;
 
-Object.keys(init).forEach(k => {
-    var [ox, oy] = rk(k);
+Object.keys(init).forEach((k, ki) => {
+    var [ox, oy, d] = rk(k);
     
-    if (runSim(ox, oy)) {
+    if (runSim(ox, oy, false, ki+2)) {
         count++;
     }
 })
