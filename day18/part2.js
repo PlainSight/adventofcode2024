bdabc2512784a5c38c722b5777262555cc8c0838
Alex <alexander.ogier@gmail.com>
2024-12-18 22:26:48 +1300

    fast part2

--- day18/part2.js
+++ day18/part2.js
@@ -6,65 +6,109 @@ var bytes = input.map(n => n.split(',').map(Number));
 
 var maxx = 6;
 var maxy = 6;
-var takeMost = 12;
 
 if (input.length > 25) {
     maxx = 70;
     maxy = 70;
-    takeMost = 1024;
 }
 
-for(var i = takeMost; i < input.length; i++) {
-    var foundPath = false;
-    var byteSegment = bytes.slice(0, Math.min(bytes.length, i));
-
-    var seen = {'0,0':1};
-    
-    const k = (x, y) => {
-        return x+','+y;
-    }
-    
-    byteSegment.forEach(bs => {
-        seen[k(bs[0], bs[1])] = 1;
-    })
-    
-    var stack = [{
-        d: 0,
-        x: 0,
-        y: 0
-    }];
-    
-    var ds = [
-        [1, 0],
-        [0, 1],
-        [-1, 0],
-        [0, -1]
-    ];
-
-    while(stack.length && !foundPath) {
-        var next = stack.shift();
-    
-        if (next.x == maxx && next.y == maxy) {
-            foundPath = true;
+const combineAttr = (a1, a2) => {
+    return [...a1, ...a2].sort().reduce((a, c) => {
+        if (!a.includes(c)) {
+            a.push(c);
         }
-    
-        ds.forEach(d => {
-            var nx = next.x + d[0];
-            var ny = next.y + d[1];
-            
-            if(nx >= 0 && ny >= 0 && nx <= maxx && ny <= maxy && !seen[k(nx, ny)]) {
-                seen[k(nx, ny)] = 2;
-                stack.push({
-                    d: next.d+1,
-                    x: nx,
-                    y: ny
-                })
-            }
-        })
+        return a;
+    }, []);
+}
+
+const groupItems = (x1, x2) => {
+    x1Set = x1;
+    x2Set = x2;
+    // find parent sets 
+    while (x1Set.parent) {
+        x1Set = x1Set.parent;
     }
 
-    if (!foundPath) {
-        console.log(input[i-1]);
+    while (x2Set.parent) {
+        x2Set = x2Set.parent;
+    }
+
+    if (x1Set != x2Set) {
+        // find largest set
+        var largestSet = x2Set;
+        var smallestSet = x1Set;
+
+        if (x1Set.size > x2Set.size) {
+            largestSet = x1Set;
+            smallestSet = x2Set;
+        }
+
+        largestSet.attr = combineAttr(largestSet.attr, smallestSet.attr);
+        largestSet.size += smallestSet.size;
+
+        smallestSet.parent = largestSet;
+    }
+}
+
+var locations = new Array((maxx+1) * (maxy+1));
+
+const k = (x, y) => {
+    return y * (maxx+1) + x;
+}
+
+var ds = [
+    [1, 0],
+    [1, 1],
+    [1, -1],
+    [-1, 0],
+    [-1, 1],
+    [-1, -1],
+    [0, 1],
+    [0, -1]
+];
+
+for(var i = 0; i < bytes.length; i++) {
+    var o = bytes[i];
+    
+    // make new set
+    var newSet = {
+        attr: [],
+        parent: null,
+        size: 1
+    }
+
+    if (o[0] == 0) {
+        newSet.attr.push(1)
+    }
+    if (o[0] == maxx) {
+        newSet.attr.push(3)
+    }
+    if (o[1] == 0) {
+        newSet.attr.push(2)
+    }
+    if (o[1] == maxy) {
+        newSet.attr.push(4)
+    }
+
+    locations[k(o[0], o[1])] = newSet;
+
+    ds.forEach(d => {
+        var nx = o[0] + d[0];
+        var ny = o[1] + d[1];
+        if (nx >= 0 && ny >= 0 && nx <= maxx && ny <= maxy) {
+            var neighbour = locations[k(nx, ny)];
+            if (neighbour) {
+                groupItems(newSet, neighbour);
+            }
+        }
+    });
+
+    var ultimateSet = newSet;
+    while (ultimateSet.parent) {
+        ultimateSet = ultimateSet.parent;
+    }
+    if (ultimateSet.attr.length > 2 || (ultimateSet.attr.length == 2 && ultimateSet.attr.reduce((a, c) => a + c, 0) % 2 == 0)) {
+        console.log(o.join(','));
         return;
     }
 }
