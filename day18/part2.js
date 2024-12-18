d3568fdb4b385963dfb41d423f224236d95f819e
Alex <alexander.ogier@gmail.com>
2024-12-19 02:54:27 +1300

    cleanup

--- day18/part2.js
+++ day18/part2.js
@@ -12,18 +12,9 @@ if (input.length > 25) {
     maxy = 70;
 }
 
-const combineAttr = (a1, a2) => {
-    return [...a1, ...a2].sort().reduce((a, c) => {
-        if (!a.includes(c)) {
-            a.push(c);
-        }
-        return a;
-    }, []);
-}
-
 const groupItems = (x1, x2) => {
-    x1Set = x1;
-    x2Set = x2;
+    x1Set = x1.set;
+    x2Set = x2.set;
     // find parent sets 
     while (x1Set.parent) {
         x1Set = x1Set.parent;
@@ -43,10 +34,13 @@ const groupItems = (x1, x2) => {
             smallestSet = x2Set;
         }
 
-        largestSet.attr = combineAttr(largestSet.attr, smallestSet.attr);
+        largestSet.attr = largestSet.attr | smallestSet.attr;
         largestSet.size += smallestSet.size;
 
         smallestSet.parent = largestSet;
+
+        x1.set = largestSet;
+        x2.set = largestSet;
     }
 }
 
@@ -67,30 +61,41 @@ var ds = [
     [0, -1]
 ];
 
+const blockedConditions = [
+    3, // horizontal    0011
+    12, // vertical     1100
+    5, // top left      0101
+    10 // bottom right  1010
+]
+
 for(var i = 0; i < bytes.length; i++) {
     var o = bytes[i];
     
     // make new set
     var newSet = {
-        attr: [],
+        attr: 0,
         parent: null,
         size: 1
     }
 
     if (o[0] == 0) {
-        newSet.attr.push(1)
+        newSet.attr |= 1; //0001
     }
     if (o[0] == maxx) {
-        newSet.attr.push(3)
+        newSet.attr |= 2; //0010
     }
     if (o[1] == 0) {
-        newSet.attr.push(2)
+        newSet.attr |= 4; //0100
     }
     if (o[1] == maxy) {
-        newSet.attr.push(4)
+        newSet.attr |= 8; //1000
     }
 
-    locations[k(o[0], o[1])] = newSet;
+    const newByte = {
+        set: newSet
+    };
+
+    locations[k(o[0], o[1])] = newByte;
 
     ds.forEach(d => {
         var nx = o[0] + d[0];
@@ -98,17 +103,13 @@ for(var i = 0; i < bytes.length; i++) {
         if (nx >= 0 && ny >= 0 && nx <= maxx && ny <= maxy) {
             var neighbour = locations[k(nx, ny)];
             if (neighbour) {
-                groupItems(newSet, neighbour);
+                groupItems(newByte, neighbour);
             }
         }
     });
 
-    var ultimateSet = newSet;
-    while (ultimateSet.parent) {
-        ultimateSet = ultimateSet.parent;
-    }
-    if (ultimateSet.attr.length > 2 || (ultimateSet.attr.length == 2 && ultimateSet.attr.reduce((a, c) => a + c, 0) % 2 == 0)) {
+    if (blockedConditions.some(bc => (bc & newByte.set.attr) == bc)) {
         console.log(o.join(','));
-        return;
+        break;
     }
 }
