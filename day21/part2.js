cc03d750f7d9a0a205ca9e55404aba8007974c93
Alexander Ogier <alexander.ogier@gmail.com>
2024-12-24 17:18:47 +1300

    clarify some sorting of paths that doesn't rely on happenstance

--- day21/part2.js
+++ day21/part2.js
@@ -32,10 +32,10 @@ const k = (x, y) => {
 }
 
 var ds = [
-    [0, 1],
-    [0, -1],
     [1, 0],
-    [-1, 0]
+    [0, 1],
+    [-1, 0],
+    [0, -1]
 ];
 
 const resolvePathToMoves = (end, startChar, endChar, map) => {
@@ -61,12 +61,20 @@ const resolvePathToMoves = (end, startChar, endChar, map) => {
         }
         current = current.parent;
     }
+
+    // how does this work?
+    // A should be last in the list always
+    // < is expensive getting to it requires 3 instructions v<< so should be first if possible 
+    // so that instructions that follow leave the "cursor" closer to A
+
+    // ^ and > are both adjacent to A
+    // however ^>A costs less than >^A after many generations
     const sortDistFromA = {
-        'A': 4,
-        'v': 2,
-        '^': 1,
+        'A': 5,
+        'v': 1,
+        '^': 2,
         '<': 0,
-        '>': 2
+        '>': 3
     }
 
     const sortOrderVert = {
@@ -87,11 +95,11 @@ const resolvePathToMoves = (end, startChar, endChar, map) => {
 
     if (map.length > 2) {
         const illegalMoves = {
-            '7': 3,
-            '4': 2,
-            '1': 1,
-            '0': 1,
-            'A': 2
+            '7': 3, // moving down 3 from 7 is illegal
+            '4': 2, // moving down 2 from 4 is illegal
+            '1': 1, // moving down 1 from 1 is illegal
+            '0': 1, // moving left once from 0 is illegal
+            'A': 2 // moving left twice from 0 is illegal
         }
     
         // left side of num pad 
@@ -104,9 +112,9 @@ const resolvePathToMoves = (end, startChar, endChar, map) => {
         }
     } else {
         const illegalMoves = {
-            '<': 1,
-            '^': 1,
-            'A': 2
+            '<': 1, // moving up from < is illegal
+            '^': 1, // moving left from ^ is illegal
+            'A': 2 // moving left twice from A is illegal
         }
     
         // left side of d pad 
@@ -119,6 +127,7 @@ const resolvePathToMoves = (end, startChar, endChar, map) => {
         }
     }
 
+    // sorting groups same direction movement into contiguous blocks
     path.sort((a, b) => sortOrder[a] - sortOrder[b]);
 
     return path;
@@ -186,19 +195,41 @@ const possiblePath = (locations, map) => {
     return finalPath;
 }
 
-
 const findCompositePath = (locations, map) => {
     return possiblePath(['A', ...locations], map);
 }
 
 var totalComplexity = 0n;
 
-var cache = {};
+const getCostAfterGenerations = (operations, generations, cache) => {
+    let buckets = {};
+    operations.forEach(o => {
+        buckets[o] = buckets[o] ?? 0n;
+        buckets[o]++;
+    })
+    for(var i = 0; i < generations; i++) {
+        let nextBuckets = {};
+        Object.entries(buckets).forEach(e => {
+            var newEntries = cache[e[0]];
+            newEntries.forEach(ne => {
+                nextBuckets[ne] = nextBuckets[ne] ?? 0n;
+                nextBuckets[ne] += e[1];   
+            });
+        });
+        buckets = nextBuckets;
+    }
+    const length = Object.entries(buckets).reduce((a, e) => {
+        return a + (BigInt(e[0].length) * e[1]);
+    }, 0n);
+    return length;
+}
 
 passwords.forEach((p, pi) => {
     const numeric = Number(/(\d+)/.exec(p.join(''))[1]);
 
     let currentPhase = findCompositePath(p, numberPadMap);
+
+    var cache = {};
 
     for(var i = 0; i < 4; i++) {
         const lastPhase = currentPhase;
@@ -226,32 +257,10 @@ passwords.forEach((p, pi) => {
         }
     }
 
-    // analyse current phase
-    // bucket elements
-
-    let buckets = currentPhase.join('').split('A').slice(0, -1).map(x => x+'A').reduce((a, c) => {
-        a[c] = a[c] ?? 0n;
-        a[c]++;
-        return a;
-    }, {});
-
     // iterate 21 more times
 
-    for(var i = 0; i < 21; i++) {
-        let nextBuckets = {};
-        Object.entries(buckets).forEach(e => {
-            var newEntries = cache[e[0]];
-            newEntries.forEach(ne => {
-                nextBuckets[ne] = nextBuckets[ne] ?? 0n;
-                nextBuckets[ne] += e[1];   
-            });
-        });
-        buckets = nextBuckets;
-    }
-
-    const length = Object.entries(buckets).reduce((a, e) => {
-        return a + (BigInt(e[0].length) * e[1]);
-    }, 0n);
+    let phaseElements = currentPhase.join('').split('A').slice(0, -1).map(x => x+'A');
+    const length = getCostAfterGenerations(phaseElements, 21, cache);
 
     totalComplexity += length * BigInt(numeric);
 });
